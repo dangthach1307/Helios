431c5a8042b784683ba83004f5174eb66719bca9
dangthach1307 <dangthach1307@gmail.com>
2023-12-05 19:57:16 +0700

    Trang  thống kê

--- admin/views/pages/dashboard/index.php
+++ admin/views/pages/dashboard/index.php
@@ -5,12 +5,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard</h1>
+                    <h1 class="m-0">Thống kê</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard</li>
+                        <li class="breadcrumb-item active">Thống kê</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
@@ -21,69 +21,70 @@
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
-            <!-- Info boxes -->
             <div class="row">
-                <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-info elevation-1"><i class="fas fa-box"></i></span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">Sản phẩm</span>
-                            <span class="info-box-number">
-                                <?= $count_product; ?>
-                            </span>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3><?= $count_product ?></h3>
+
+                            <p>Sản phẩm</p>
                         </div>
-                        <!-- /.info-box-content -->
-                    </div>
-                    <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-                <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box mb-3">
-                        <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-list"></i></span>
-
-                        <div class="info-box-content">
-                            <span class="info-box-text">Danh mục</span>
-                            <span class="info-box-number"><?= $count_category; ?></span>
+                        <div class="icon">
+                            <i class="fa fa-box"></i>
                         </div>
-                        <!-- /.info-box-content -->
+                        <a href="index.php?option=product" class="small-box-footer">Xem thêm <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
-                    <!-- /.info-box -->
                 </div>
-                <!-- /.col -->
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3><?= $count_post ?></h3>
 
-                <!-- fix for small devices only -->
-                <div class="clearfix hidden-md-up"></div>
-
-                <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box mb-3">
-                        <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
-
-                        <div class="info-box-content">
-                            <span class="info-box-text">Đơn hàng</span>
-                            <span class="info-box-number">10</span>
+                            <p>Bài viết</p>
                         </div>
-                        <!-- /.info-box-content -->
-                    </div>
-                    <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-                <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box mb-3">
-                        <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
-
-                        <div class="info-box-content">
-                            <span class="info-box-text">Thành viên</span>
-                            <span class="info-box-number"><?= $count_user; ?></span>
+                        <div class="icon">
+                            <i class="fa fa-book"></i>
                         </div>
-                        <!-- /.info-box-content -->
+                        <a href="index.php?option=post" class="small-box-footer">Xem thêm <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
-                    <!-- /.info-box -->
                 </div>
-                <!-- /.col -->
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3><?= $count_user ?></h3>
+                            <p>Số lượng người dùng</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-user"></i>
+                        </div>
+                        <a href="index.php?option=customer" class="small-box-footer">Xem thêm <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3><?= $count_order ?></h3>
+
+                            <p>Đơn hàng</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-shopping-cart"></i>
+                        </div>
+                        <a href="index.php?option=order" class="small-box-footer">Xem thêm <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
             </div>
+            <!-- Info boxes -->
             <!-- /.row -->
             <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-6">
                     <div class="card">
                         <div class="card-header">
                             <h5 class="card-title">Thống kê sản phẩm</h5>
@@ -91,18 +92,6 @@
                                 <button type="button" class="btn btn-tool" data-card-widget="collapse">
                                     <i class="fas fa-minus"></i>
                                 </button>
-                                <!-- <div class="btn-group">
-                                    <button type="button" class="btn btn-tool dropdown-toggle" data-toggle="dropdown">
-                                        <i class="fas fa-wrench"></i>
-                                    </button>
-                                    <div class="dropdown-menu dropdown-menu-right" role="menu">
-                                        <a href="#" class="dropdown-item">Action</a>
-                                        <a href="#" class="dropdown-item">Another action</a>
-                                        <a href="#" class="dropdown-item">Something else here</a>
-                                        <a class="dropdown-divider"></a>
-                                        <a href="#" class="dropdown-item">Separated link</a>
-                                    </div>
-                                </div> -->
                                 <button type="button" class="btn btn-tool" data-card-widget="remove">
                                     <i class="fas fa-times"></i>
                                 </button>
@@ -112,7 +101,7 @@
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-12">
-                                    <div id="statistic_product_by_category"></div>
+                                    <div id="statistic_product_by_category" style="width:100%; max-width:600px; height:500px;"></div>
                                     <!-- /.chart-responsive -->
                                 </div>
                                 <!-- /.col -->
@@ -124,26 +113,14 @@
                     <!-- /.card -->
                 </div>
                 <!-- /.col -->
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <div class="card">
                         <div class="card-header">
-                            <h5 class="card-title">Thống kê danh mục</h5>
+                            <h5 class="card-title">Thống kê đơn hàng</h5>
                             <div class="card-tools">
                                 <button type="button" class="btn btn-tool" data-card-widget="collapse">
                                     <i class="fas fa-minus"></i>
                                 </button>
-                                <!-- <div class="btn-group">
-                                    <button type="button" class="btn btn-tool dropdown-toggle" data-toggle="dropdown">
-                                        <i class="fas fa-wrench"></i>
-                                    </button>
-                                    <div class="dropdown-menu dropdown-menu-right" role="menu">
-                                        <a href="#" class="dropdown-item">Action</a>
-                                        <a href="#" class="dropdown-item">Another action</a>
-                                        <a href="#" class="dropdown-item">Something else here</a>
-                                        <a class="dropdown-divider"></a>
-                                        <a href="#" class="dropdown-item">Separated link</a>
-                                    </div>
-                                </div> -->
                                 <button type="button" class="btn btn-tool" data-card-widget="remove">
                                     <i class="fas fa-times"></i>
                                 </button>
@@ -153,7 +130,7 @@
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-12">
-                                    <div id="statistic_category"></div>
+                                    <div id="statistic_order_by_stage" style="width:100%; max-width:100%; height:500px;"></div>
                                     <!-- /.chart-responsive -->
                                 </div>
                                 <!-- /.col -->
@@ -175,7 +152,7 @@
 <script type="text/javascript">
     // Load google charts
     google.charts.load('current', {
-        'packages': ['corechart']
+        'packages': ['piechart']
     });
     google.charts.setOnLoadCallback(drawChart);
 
@@ -197,7 +174,7 @@
         };
 
         // Display the chart inside the <div> element with id="piechart"
-        var chart = new google.visualization.ColumnChart(document.getElementById('statistic_product_by_category'));
+        var chart = new google.visualization.PieChart(document.getElementById('statistic_product_by_category'));
         chart.draw(data, options);
     }
 </script>
@@ -232,6 +209,52 @@
 
         // Display the chart inside the <div> element with id="piechart"
         var chart = new google.visualization.ColumnChart(document.getElementById('statistic_category'));
+        chart.draw(data, options);
+    }
+</script>
+<script type="text/javascript">
+    // Load google charts
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+    google.charts.setOnLoadCallback(drawChart);
+
+    // Draw the chart and set the chart values
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Trạng thái', 'Đơn hàng'],
+            <?php
+            foreach ($order_statitic as $item) {
+                extract($item);
+                if ($trang_thai_don_hang == 1) {
+                    $trang_thai_don_hang = "Chờ duyệt";
+                } elseif ($trang_thai_don_hang == 2) {
+                    $trang_thai_don_hang = "Đang xử lý";
+                } elseif ($trang_thai_don_hang == 3) {
+                    $trang_thai_don_hang = "Huỷ đơn";
+                } elseif ($trang_thai_don_hang == 4) {
+                    $trang_thai_don_hang = "Đang giao";
+                } elseif ($trang_thai_don_hang == 5) {
+                    $trang_thai_don_hang = "Giao thành công";
+                } elseif ($trang_thai_don_hang == 6) {
+                    $trang_thai_don_hang = "Trả hàng/Đổi hàng";
+                } elseif ($trang_thai_don_hang == 7) {
+                    $trang_thai_don_hang = "Hoàn thành";
+                }
+                echo "['$trang_thai_don_hang', $so_luong],";
+            }
+            ?>
+        ]);
+
+        // Optional; add a title and set the width and height of the chart
+        var options = {
+            'title': 'Thống kê đơn hàng',
+            is3D: true
+
+        };
+
+        // Display the chart inside the <div> element with id="piechart"
+        var chart = new google.visualization.ColumnChart(document.getElementById('statistic_order_by_stage'));
         chart.draw(data, options);
     }
 </script>
