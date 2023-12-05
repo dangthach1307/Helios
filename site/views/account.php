d5000dd9e04467231789ea42e236ddd171e478b9
dangthach1307 <dangthach1307@gmail.com>
2023-12-05 16:58:17 +0700

    Sửa lại chút xíu nữa

--- site/views/account.php
+++ site/views/account.php
@@ -12,7 +12,6 @@
                                 <div class="col-md-5">
                                     <div class="text-center">
                                         <img class="img img-fluid img-circle" style="margin-top: 15px;" height="250px" width="250px" src="../public/images/user/<?= $_SESSION["user"]["img"] ?>">
-
                                         <div class="service-desc text-center" style="margin: 15px 0 0 30px;display:flex;width:100%">
                                             <h4 style="margin-top: 30px; ">Cấp thành viên:</h4>
                                             <img class="img-responsive" style="margin-top: -10px;width:50%;height:50%" src="../public/images/rank/<?= $rank_img ?>">
@@ -53,7 +52,7 @@
                                         <div class="service-icon"> <i class="fa fa-gift"></i> </div>
                                         <div class="service-desc">
                                             <h4>Lịch sử đơn hàng</h4>
-                                            <p>Track, return, or buy things again</p>
+                                            <p>Quản lý đơn hàng của bạn</p>
                                         </div>
                                     </div>
                                 </div>
@@ -66,7 +65,20 @@
                                         <div class="service-icon"> <i class="fa fa-user"></i> </div>
                                         <div class="service-desc">
                                             <h4>Cập nhật thông tin</h4>
-                                            <p>Edit login, name, and mobile number</p>
+                                            <p>Quản lý thông tin của bạn</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm-12 col-md-12 col-xs-12">
+                            <a href="?option=user&act=account-detail">
+                                <div class="account-box">
+                                    <div class="service-box">
+                                        <div class="service-icon"> <i class="fa fa-heart"></i> </div>
+                                        <div class="service-desc">
+                                            <h4>Danh sách yêu thích</h4>
+                                            <p>Quản lý danh sách yêu thích của bạn</p>
                                         </div>
                                     </div>
                                 </div>
